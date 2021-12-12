83a13498e0641c3f6f0e2b8ce2ac7ad405a78d4b
Администратор <danielstudentbsu@gmail.com>
2021-12-12 23:19:36 +0300

    Initial commit implement main add graphics

---
+++ src/bsu/rfe/java/group7/lab4/Danilkov/var4/Main.java
@@ -0,0 +1,13 @@
+package bsu.rfe.java.group7.lab4.Danilkov.var4;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
